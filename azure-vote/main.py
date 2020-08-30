1b6d2fad4df5e3393341bac94d813887b6dc1104
Prashant Kumar <pk611886@gmail.com>
2020-08-30 09:04:03 +0530

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -7,23 +7,56 @@ import sys
 import logging
 from datetime import datetime
 
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+
 # App Insights
 # TODO: Import required libraries for App Insights
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
+
 
 # Logging
 #logger = # TODO: Setup logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # Metrics
 #exporter = # TODO: Setup exporter
+exporter = metrics_exporter.new_metrics_exporter(
+  enable_standard_metrics=True,
+  connection_string='InstrumentationKey=7a3a0e31-6c79-479f-8b87-32cab3acead2')
+
 
 # Tracing
 #tracer = # TODO: Setup tracer
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=7a3a0e31-6c79-479f-8b87-32cab3acead2'),
+    sampler=ProbabilitySampler(1.0),
+)
 
 app = Flask(__name__)
 
 # Requests
 #middleware = # TODO: Setup flask middleware
-
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=7a3a0e31-6c79-479f-8b87-32cab3acead2"),
+    sampler=ProbabilitySampler(rate=1.0),
+)
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
 
@@ -61,8 +94,11 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
+        tracer.span("Cats is voted")
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
+        tracer.span("Dogs is voted")
+
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -77,10 +113,11 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
-
+            logger.info('Cats Vote : %s' %vote1)
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
+            logger.info('Dogs Vote : %s' %vote2)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
@@ -99,6 +136,6 @@ def index():
 
 if __name__ == "__main__":
     # comment line below when deploying to VMSS
-    # app.run() # local
+    #app.run() # local
     # uncomment the line below before deployment to VMSS
     app.run(host='0.0.0.0', threaded=True, debug=True) # remote
